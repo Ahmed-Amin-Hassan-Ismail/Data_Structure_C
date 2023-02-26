aec4e2895bf69c21b3ac846af5317d4ae68a592c
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-02-26 16:37:47 +0200

    update linked list name

--- AddLinkedList/BasicLinkedList.cpp
+++ AddLinkedList/BasicLinkedList.cpp
@@ -67,11 +67,45 @@ public:
         length++;
     }
 
-    
+    void insertAtPosition(int post, int element)
+    {
+        if (post < 0 || post > length)
+        {
+            cout << "Out Of Range";
+        }
+        else
+        {
+            Node *newNode = new Node;
+            newNode->item = element;
+
+            if (post == 0)
+            {
+                insertFirst(element);
+            }
+            else if (post == length)
+            {
+                inserLast(element);
+            }
+            else
+            {
+
+                Node *current = first;
+
+                for (size_t i = 0; i < post; i++)
+                {
+                    current = current->next;
+                }
+
+                newNode->next = current->next;
+                current->next = newNode;
+                length++;
+            }
+        }
+    }
 };
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    
     return 0;
 }
