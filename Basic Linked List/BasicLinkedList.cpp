8b40b54cb3cbdebfa72f5b7dc0a591e63d4a6b97
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-02-26 16:35:38 +0200

    implement Add new element whether first , last or middle

--- Basic Linked List/BasicLinkedList.cpp
+++ Basic Linked List/BasicLinkedList.cpp
@@ -23,6 +23,49 @@ public:
         length = 0;
     }
 
+    bool isEmpty()
+    {
+        return length == 0;
+    }
+
+    void insertFirst(int elelemt)
+    {
+        Node *newNode = new Node;
+        newNode->item = elelemt;
+
+        if (length == 0)
+        {
+
+            first = last = newNode;
+            newNode->next = NULL;
+        }
+        else
+        {
+            newNode->next = first;
+            first = newNode;
+        }
+
+        length++;
+    }
+
+    void inserLast(int element)
+    {
+        Node *newNode = new Node;
+        newNode->item = element;
+
+        if (length == 0)
+        {
+            first = last = newNode;
+            newNode->next = NULL;
+        }
+        else
+        {
+            last->next = newNode;
+            newNode->next = NULL;
+            last = newNode;
+        }
+        length++;
+    }
 
     
 };
